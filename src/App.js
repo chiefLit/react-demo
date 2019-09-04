1f23d52ebf00818acd22fa635d13117c84441c6a
chief <liting@chief-mac.local>
2019-09-04 11:05:24 +0800

    -

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Input, List } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Input 
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        <List
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={this.state.list}
+          renderItem={item => <List.Item>{item}</List.Item>}
+        />
+      </div>
+    );
+  }
+
+  handleInputChange(e) {
+    console.log(e)
+  }
 }
 
-export default App;
+export default TodeList;
